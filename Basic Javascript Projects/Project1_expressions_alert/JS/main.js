1f56ddef19c81be2a24e89f087f6fe9dddd3131e
DrePorter2 <101455766+DrePorter2@users.noreply.github.com>
2022-03-30 16:13:30 -0400

    Edit

--- Basic Javascript Projects/Project1_expressions_alert/JS/main.js
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -1,4 +1,11 @@
-window.alert("Is everyone doing well?");
-document.write('I know I am');
-var A = "This is a string";
-document.write(A);
+window.alert("Is everyone doing well?"); // This creates a display box when the site loads
+document.write('I know I am'); // This adds text to the document
+var A = "This is a string"; // This assigns the text to the letter A
+document.write(A); // This writes out the text assigned to the variable A
+var B = "I/'m writing this as a test"; // This assigns the text to the letter B
+document.write(B); // This writes out the text assigned to the variable B
+document.write("I just learned how to concatenate data," // This adds connected text the the document
++ "Hopefully I did it correctly");
+var Cars = "Non luxury", Honda = "Accord", Toyota = "Camry", Nissan = "Altima" // This assigns multiple text to multiple variables
+document.write(Toyota);
+document.write(87 - 42); // This assigns the expression 87 minus 42
