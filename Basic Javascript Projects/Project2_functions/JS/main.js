1f235dbe9942748dfd5ba79bcb8ef40d2d4354fa
DrePorter2 <101455766+DrePorter2@users.noreply.github.com>
2022-03-30 16:37:09 -0400

    Update

---
+++ Basic Javascript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+function My_First_Function() { //This function allows the html document to call for the text when the button is clicked
+    var str="This is the button text!";
+    document.getElementById("Button_Text").innerHTML = str;
+}
+
+function myfunction() { //This function allows the html document to call for the concatenated text when the button is clicked
+    var sentence = "I/'m enjoying this course";
+    sentence += "so much!";
+    document.getElementById(concatenate).innerHTML = sentence;
+}
